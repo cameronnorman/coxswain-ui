52eecbb2d250f1e621ab887c3631f77981a767d1
Chrys-Nicolaides <65061438+Chrys-Nicolaides@users.noreply.github.com>
2020-06-22 21:13:20 +0200

    Feature/ui (#4)
    * Added requirements for initial UI layout
    
    * Bump websocket-extensions from 0.1.3 to 0.1.4
    
    Bumps [websocket-extensions](https://github.com/faye/websocket-extensions-node) from 0.1.3 to 0.1.4.
    - [Release notes](https://github.com/faye/websocket-extensions-node/releases)
    - [Changelog](https://github.com/faye/websocket-extensions-node/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/faye/websocket-extensions-node/compare/0.1.3...0.1.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Cameron Norman <cameronrossnorman@gmail.com>

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Backdrop.css'
+
+function Backdrop() {
+  return (<div className="Backdrop">
+      <div className="left">''</div>
+      <div className="right">''</div>
+
+  </div>);
+}
+
+export default Backdrop
